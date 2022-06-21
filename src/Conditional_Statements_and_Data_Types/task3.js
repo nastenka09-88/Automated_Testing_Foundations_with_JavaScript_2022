0fa025fc51ce9be79f1279704fc4ab949d831094
Anastasya Dzeraviannik <nastenka09-88@tut.by>
2022-06-22 00:54:47 +0300

    Tasks Conditional Statements and Data Types - done

---
+++ src/Conditional_Statements_and_Data_Types/task3.js
@@ -0,0 +1,9 @@
+let string = "Hello!";
+let boolean = false;
+let number = 115;
+let div_string_boolean = string / boolean;
+let div_string_number = string / number;
+let div_number_boolean = number / boolean;
+console.log(div_string_boolean);
+console.log(div_string_number);
+console.log(div_number_boolean);
